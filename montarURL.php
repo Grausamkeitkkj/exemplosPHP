83166af88f7281558e58ca415a66db39e36b4006
Diogo Schultz <diogorschultz2@gmail.com>
2026-01-09 09:27:36 -0300

    reformulando exemplo de url

--- montarURL.php
+++ montarURL.php
@@ -1,19 +1,10 @@
 <?php
-    $categoria = htmlspecialchars($assuntoProntuario['nome'] ?? 'Categoria não informada');
 
-    $idCategoria = $assuntoProntuario['id'] ?? null;
+    $queryParams = http_build_query([
+        'variavel1' => $variavel1,
+        'variavel2' => $variavel2,
+    ], '', '&', PHP_QUERY_RFC3986);
 
-    if ($idProntuarioBase64 !== null && $idCategoria !== null) {
-        $encodedCategoriaId = base64_encode((string) $idCategoria);
+    $link = 'caminho_do_arquivo.php?' . $queryParams;
 
-        $queryParams = http_build_query([
-            'idProntuario' => $idProntuarioBase64,
-            'idCategoriaProntuario' => $encodedCategoriaId,
-        ], '', '&', PHP_QUERY_RFC3986);
-
-        $link = 'assunto_prontuario.php?' . $queryParams;
-        echo '<a href="' . $link . '" class="text-primary">' . $categoria . '</a>';
-    } else {
-        echo $categoria;
-    }
 ?>
